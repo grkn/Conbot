d062e0bb32b66526faa7927f428e383c8abbba03
turkana78 <turkanayar@gmail.com>
2018-02-07 14:03:10 +0300

    comments

--- mongo/mongoQueries.js
+++ mongo/mongoQueries.js
@@ -1,46 +1,37 @@
-
-
 var mongoQueries = class MongoQueries {
-
   constructor(db){
     this.db = db;
   }
-
-  createCollection(collectionName,callback){
+  createCollection(collectionName, callback){
     this.db.db('conbot').createCollection(collectionName, function(err, res) {
       if (err) throw err;
-      callback(res,err);
+      callback(res, err);
     });
   }
-
-  insertMany(collectionName,obj,callback){
+  insertMany(collectionName, obj, callback){
     this.db.db("conbot").collection(collectionName).insertMany(obj, function(err, res) {
       if (err) throw err;
-      callback({resp:"OK"},obj);
+      callback({res:"OK"}, obj);
     });
   }
-
-  insertOne(collectionName,obj,callback){
+  insertOne(collectionName, obj, callback){
     this.db.db("conbot").collection(collectionName).insertOne(obj, function(err, res) {
       if (err) throw err;
-      callback({resp:"OK"},obj);
+      callback({res:"OK"}, obj);
     });
   }
-  find(collectionName,callback){
-    this.db.db("conbot").collection(collectionName).find({}).toArray(function(err, result) {
+  find(collectionName, callback){
+    this.db.db("conbot").collection(collectionName).find({}).toArray(function(err, res) {
      if (err) throw err;
-      callback(result);
+      callback(res);
     });
   }
-
-  findByQuery(collectionName,query,callback){
-    this.db.db("conbot").collection(collectionName).find(query).toArray(function(err, result) {
+  findByQuery(collectionName, query, callback){
+    this.db.db("conbot").collection(collectionName).find(query).toArray(function(err, res) {
      if (err) throw err;
-      callback(result);
+      callback(res);
     });
   }
-
-
 }
 
 module.exports = mongoQueries;
